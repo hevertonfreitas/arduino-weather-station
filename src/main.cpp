5df299d201f46223a5a63978ecca38dfd72f81fe
Heverton Coneglian de Freitas <hevertonconeglian@gmail.com>
2023-10-07 16:28:20 -0300

    Refatora constantes

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,18 @@
 #include <LiquidCrystal.h>
 #include "DHT.h"
 
+#define LCDRS 12
+#define LCDENABLE 11
+#define LCDD0 5
+#define LCDD1 4
+#define LCDD2 3
+#define LCDD3 2
+
 #define DHTPIN A0
 #define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(LCDRS, LCDENABLE, LCDD0, LCDD1, LCDD2, LCDD3);
 
 void setup() {
     lcd.begin(16, 2);
@@ -32,11 +39,13 @@ void loop() {
 
     if (!isnan(t) && !isnan(h)) {
         lcd.setCursor(0, 0);
-        lcd.print("Temperatura: ");
+        lcd.print("C: ");
         lcd.print(t);
+        lcd.print(" graus");
 
         lcd.setCursor(0, 1);
-        lcd.print("Umidade: ");
+        lcd.print("UR: ");
         lcd.print(h);
+        lcd.print("%");
     }
 }
