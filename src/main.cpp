68c404b86ff7e8764b6492c3c0903c5bd6ecc94b
Heverton Coneglian de Freitas <hevertonconeglian@gmail.com>
2023-11-04 16:12:44 -0300

    wifi connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 #include "SoftwareSerial.h"
-#include "ESP_AT_Lib.h"
 #include "SPI.h"
 #include "Wire.h"
 #include "LiquidCrystal.h"
@@ -17,18 +16,51 @@
 #define DHTPIN A0
 #define DHTTYPE DHT11
 
-#define SSID ""
-#define PASSWORD ""
-
 #define ESP_AT_BAUD 115200
 #define SERIAL_BAUD 9600
 
 SoftwareSerial ESP_Serial(9, 10); // TX, RX
-ESP8266 wifi(&ESP_Serial);
 
 DHT dht(DHTPIN, DHTTYPE);
 LiquidCrystal lcd(LCDRS, LCDENABLE, LCDD0, LCDD1, LCDD2, LCDD3);
 RTC_DS1307 rtc;
+
+bool startWiFi() {
+    ESP_Serial.flush();
+    ESP_Serial.println("AT");
+    delay(2000);
+
+    if (ESP_Serial.find("OK")) {
+        Serial.println("Comunicacao com modulo ESP8266: OK");
+    } else {
+        Serial.println("Erro no modulo ESP8266");
+    }
+
+    Serial.println("Conectando wi-fi...");
+    Serial.println("AT+CWMODE=1");
+    ESP_Serial.println("AT+CWMODE=1");
+    delay(2000);
+    ESP_Serial.flush();
+
+    Serial.println("Desabilitando multilpas conexões");
+    Serial.println("AT+CIPMUX=0");
+    ESP_Serial.println("AT+CIPMUX=0");
+    delay(2000);
+    ESP_Serial.flush();
+
+    String wifiConnectString = "AT+CWJAP=\"ssid\",\"password\"";
+    Serial.println(wifiConnectString);
+    ESP_Serial.println(wifiConnectString);
+    delay(5000);
+
+    if (ESP_Serial.find("OK")) {
+        Serial.println("Conectado com sucesso!");
+        return true;
+    } else {
+        Serial.println("Falha na conexao!");
+        return false;
+    }
+}
 
 void setup() {
     Serial.begin(SERIAL_BAUD);
@@ -36,7 +68,7 @@ void setup() {
 
     lcd.begin(16, 2);
     lcd.clear();
-    lcd.print("inicializando...");
+    lcd.print("Inicializando...");
 
     if (!rtc.begin()) {
         Serial.println("Couldn't find RTC");
@@ -46,19 +78,7 @@ void setup() {
 
     dht.begin();
 
-    if (wifi.setOprToStation()) {
-        Serial.println("Set STA Mode OK");
-    } else {
-        Serial.println("Set STA Mode failed");
-    }
-
-    if (wifi.joinAP(SSID, PASSWORD)) {
-        Serial.println("Connect to WiFi OK");
-        Serial.print("IP: ");
-        Serial.println(wifi.getLocalIP().c_str());
-    } else {
-        Serial.println("Connect to WiFi failed");
-    }
+    startWiFi();
 
     lcd.clear();
 }
@@ -69,6 +89,32 @@ float temperature() {
 
 float humidity() {
     return dht.readHumidity();
+}
+
+void updateSensors(uint32_t unixtime, float temperature, float humidity) {
+    String host = "";
+
+    String s_unixtime = String(unixtime);
+    String s_temperature = String(temperature);
+    String s_humidity = String(humidity);
+
+    String get = "GET /update?time=%unixtime%&temperature=%temperature%&humidity=%humidity% HTTP/1.1\r\nHost: weather.hevertonfreitas.com.br\r\nConnection: keep-alive\r\nUser-Agent: Mozilla/5.0\r\n\r\n";
+    get.replace("%unixtime%", s_unixtime);
+    get.replace("%temperature%", s_temperature);
+    get.replace("%humidity%", s_humidity);
+
+    String startConnection = "AT+CIPSTART=\"TCP\",\"" + host + "\",80";
+    Serial.println(startConnection);
+    ESP_Serial.println(startConnection);
+    delay(1000);
+
+    String dataCommand = "AT+CIPSEND=" + String(get.length());
+    Serial.println(dataCommand);
+    ESP_Serial.println(dataCommand);
+    delay(1000);
+
+    Serial.println(get);
+    ESP_Serial.print(get);
 }
 
 void loop() {
@@ -99,4 +145,8 @@ void loop() {
     }
 
     delay(1000);
+
+    if (now.minute() % 5 == 0 && now.second() == 0) {
+        updateSensors(now.unixtime(), t, h);
+    }
 }
